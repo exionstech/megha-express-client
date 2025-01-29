e48a081ce49355d6abc2bd73b968cc0703c5961d
gourab8389 <deyg6988@gmail.com>
2025-01-30 02:13:45 +0530

    Add Testimonials component and Carousel for enhanced user engagement; update package.json to include react-icons and adjust Footer spacing

---
+++ src/app/(landing)/_components/testimonials.tsx
@@ -0,0 +1,40 @@
+const slideData = [
+    {
+      title: "Mystic Mountains",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Urban Dreams",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Neon Nights",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Desert Whispers",
+      button: "Explore Component",
+      src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+
+
+const Testimonials = () => {
+  return (
+    <section className='w-full bg-customBlue py-8'>
+        <div className="w-full max-w-screen-2xl px-5 md:px-14 mx-auto flex flex-col gap-5 items-center">
+        <div className="w-full text-center flex flex-col gap-3">
+        <h1 className="text-3xl lg:text-4xl 2xl:text-5xl font-medium text-customBlack select-none pointer-events-none">
+          What You Can Do With Megha Express
+        </h1>
+        <p className='text-red'>See what our clients say about our service, in-depth understanding, and timely deliveries.</p>
+      </div>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonials
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,6 +4,7 @@ import Services from "./_components/services";
 import ChooseUs from "./_components/choose-us";
 import FaqSection from "./_components/faq";
 import UniqueValue from "./_components/unique-value";
+import Testimonials from "./_components/testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Services/>
       <ChooseUs/>
       <UniqueValue/>
+      <Testimonials/>
       <FaqSection/>
     </MaxWrapper>
   );
