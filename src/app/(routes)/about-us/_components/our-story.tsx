dd7a4993ae9c4395ca77d81cb7b153cf365f53dc
gourab8389 <deyg6988@gmail.com>
2025-02-01 01:14:54 +0530

    Add Our Story component and update About Us page layout

---
+++ src/app/(routes)/about-us/_components/our-story.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+
+const OurStory = () => {
+  return (
+    <section className="w-full max-w-screen-2xl mx-auto px-5 md:px-14 flex md:flex-row flex-col items-center gap-10">
+      <div className="md:w-[50%] w-full flex flex-col gap-5 text-customBlack text-center md:text-start select-none pointer-events-none">
+        <h1 className="text-3xl lg:text-4xl 2xl:text-5xl font-medium text-customBlack">
+          Our Story
+        </h1>
+        <span>
+          Megha Express was founded with a vision to revolutionize the shipping
+          industry by making deliveries faster, safer, and more reliable. What
+          started as a small initiative has grown into a trusted logistics
+          partner for businesses and individuals worldwide. Driven by innovation
+          and a passion for excellence, we have built a platform that simplifies
+          the shipping process, ensuring convenience at every step.
+        </span>
+        <span>
+          From our early days, we have focused on leveraging cutting-edge
+          technology, real-time tracking, and customer-centric solutions to meet
+          the evolving demands of modern logistics. Whether it's a small parcel
+          or bulk shipments, we take pride in delivering with precision and
+          care.
+        </span>
+        <span>
+          As we continue to grow, our commitment remains the same—providing
+          seamless shipping experiences that empower businesses and connect
+          people globally. At Megha Express, every delivery is a promise
+          fulfilled.
+        </span>
+      </div>
+      <div className="md:w-[50%] w-full flex items-center justify-center">
+        <Image
+          src="/about-us/story.png"
+          width={450}
+          height={450}
+          alt="Our Story"
+          className="rounded-xl shrink-0 overflow-hidden object-cover"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default OurStory;
--- src/app/(routes)/about-us/page.tsx
+++ src/app/(routes)/about-us/page.tsx
@@ -1,11 +1,14 @@
 import { MaxWrapper } from '@/components/shared/max-wrapper'
 import React from 'react'
 import AboutHero from './_components/about-hero'
+import OurStory from './_components/our-story'
 
 const AboutUs = () => {
   return (
     <MaxWrapper className='flex flex-col gap-10'>
         <AboutHero/>
+        <OurStory/>
+        {/* <OurMisson/> */}
     </MaxWrapper>
   )
 }
