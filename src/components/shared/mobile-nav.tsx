7bb6a911b5101ba4120d4c1bd63d6add59f7caa6
gourab8389 <deyg6988@gmail.com>
2025-01-31 17:13:03 +0530

    Refactor MobileNav component to include Logo and adjust layout for improved structure and visual hierarchy

--- src/components/shared/mobile-nav.tsx
+++ src/components/shared/mobile-nav.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 import { AlignJustify } from 'lucide-react';
 import NavitemSection from './navitem-section';
+import Logo from './logo';
 
 const MobileNav = () => {
   return (
@@ -9,7 +10,8 @@ const MobileNav = () => {
         <SheetTrigger>
         <AlignJustify className="h-8 w-8 shrink-0" />
       </SheetTrigger>
-      <SheetContent className='py-20' side={"left"}>
+      <SheetContent className='py-20 flec flex-col gap-10' side={"left"}>
+        <Logo/>
         <NavitemSection/>
       </SheetContent>
     </Sheet>
