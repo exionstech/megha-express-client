bf2940750179f397e2cdea71de93dd9238bcb051
gourab8389 <deyg6988@gmail.com>
2025-02-01 20:31:26 +0530

    Add Ship Now component to Track Package page; integrate with existing layout

--- src/app/(routes)/track-package/page.tsx
+++ src/app/(routes)/track-package/page.tsx
@@ -2,6 +2,8 @@ import { MaxWrapper } from '@/components/shared/max-wrapper'
 import React from 'react'
 import TrackOrder from './_components/track-order'
 import OverView from './_components/over-view'
+import ShipNow from './_components/ship-now'
+
 
 
 const AboutUs = () => {
@@ -9,6 +11,7 @@ const AboutUs = () => {
     <MaxWrapper className='flex flex-col gap-10 min-h-screen'>
         <TrackOrder/>
         <OverView/>
+        <ShipNow/>
     </MaxWrapper>
   )
 }
