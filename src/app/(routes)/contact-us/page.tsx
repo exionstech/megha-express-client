81aa282f1636f1060cdbf2667b45823242fd13e6
gourab8389 <deyg6988@gmail.com>
2025-02-01 20:41:01 +0530

    Add ContactHero component to Contact Us page; update layout and styles for improved presentation

--- src/app/(routes)/contact-us/page.tsx
+++ src/app/(routes)/contact-us/page.tsx
@@ -1,10 +1,11 @@
 import { MaxWrapper } from '@/components/shared/max-wrapper'
+import ContactHero from './_components/contact-hero'
 
 
 const AboutUs = () => {
   return (
     <MaxWrapper className='flex flex-col gap-10 min-h-screen'>
-        contact us
+        <ContactHero/>
     </MaxWrapper>
   )
 }
