0f2fa426778313b307d32385cf5b7cd243c98aa8
gourab8389 <deyg6988@gmail.com>
2025-02-10 18:46:52 +0530

    Add ShipOverView component to display shipping statistics; integrate into ShipWithUs page

--- src/app/(routes)/ship-with-us/page.tsx
+++ src/app/(routes)/ship-with-us/page.tsx
@@ -1,11 +1,13 @@
 import { MaxWrapper } from '@/components/shared/max-wrapper'
 import React from 'react'
 import ShipWithUsHero from './_components/ship-hero'
+import ShipOverView from './_components/ship-overview'
 
 const ShipWithUsPage = () => {
   return (
     <MaxWrapper className='flex flex-col min-h-screen'>
         <ShipWithUsHero/>
+        <ShipOverView/>
     </MaxWrapper>
   )
 }
