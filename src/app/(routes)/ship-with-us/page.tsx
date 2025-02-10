17464488b8ac290ebaab5840c5010b5e113b3b34
gourab8389 <deyg6988@gmail.com>
2025-02-10 17:50:54 +0530

    Add ShipWithUs layout and page components for new route

---
+++ src/app/(routes)/ship-with-us/page.tsx
@@ -0,0 +1,12 @@
+import { MaxWrapper } from '@/components/shared/max-wrapper'
+import React from 'react'
+
+const ShipWithUsPage = () => {
+  return (
+    <MaxWrapper className='flex flex-col min-h-screen'>
+        hello
+    </MaxWrapper>
+  )
+}
+
+export default ShipWithUsPage
