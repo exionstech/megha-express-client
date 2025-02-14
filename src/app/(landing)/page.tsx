e42fe34c4986882632a42bc85a5c9de8494349f9
gourab8389 <deyg6988@gmail.com>
2025-02-15 01:42:11 +0530

    Add ShipChooseUs component to landing page; update layout and content for improved user engagement

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,17 +1,15 @@
 import { MaxWrapper } from "@/components/shared/max-wrapper";
 import Hero from "./_components/hero";
-import Services from "./_components/services";
-import ChooseUs from "./_components/choose-us";
 import FaqSection from "./_components/faq";
 import UniqueValue from "./_components/unique-value";
 import Testimonials from "./_components/testimonials";
+import ShipChooseUs from "./_components/ship-choose-us";
 
 export default function Home() {
   return (
     <MaxWrapper className="flex flex-col gap-16">
       <Hero/>
-      <Services/>
-      <ChooseUs/>
+      <ShipChooseUs/>
       <UniqueValue/>
       <Testimonials/>
       <FaqSection/>
