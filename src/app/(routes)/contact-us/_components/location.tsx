7974028539dba899a2c44526bcd9949071617d53
Suman Mondal <mondalsumanrock20@gmail.com>
2025-02-22 17:02:39 +0530

    Update Google Maps iframe source in Location component for accuracy

--- src/app/(routes)/contact-us/_components/location.tsx
+++ src/app/(routes)/contact-us/_components/location.tsx
@@ -5,7 +5,7 @@ const Location = () => {
     <div className='w-full max-w-screen-2xl mx-auto px-5 md:px-14 overflow-hidden'>
       <div className="w-full rounded-lg overflow-hidden shadow-lg aspect-[3/2] md:aspect-[3/1]">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3687.656501950976!2d88.39082479999999!3d22.441950999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDI2JzMxLjAiTiA4OMKwMjMnMjcuMCJF!5e0!3m2!1sen!2sin!4v1738852903454!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.6357365110402!2d88.3895521756279!3d22.442732137717044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0271ed79a4cb81%3A0xf327322ba16fe03a!2sVivekananda%20Marg%2C%20Ramchandrapur%2C%20Narendrapur%2C%20West%20Bengal%20700153!5e0!3m2!1sen!2sin!4v1740223834522!5m2!1sen!2sin"
                 className="w-full h-full"
                 style={{ border: 0 }}
                 loading="lazy"
