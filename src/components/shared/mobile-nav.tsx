e6655a678253762c5368ff14be6231e8463f74b8
gourab8389 <deyg6988@gmail.com>
2025-02-25 23:58:30 +0530

    Implement mobile navigation state management; close menu on route change

--- src/components/shared/mobile-nav.tsx
+++ src/components/shared/mobile-nav.tsx
@@ -1,13 +1,21 @@
+"use client"
 import React from 'react'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 import { AlignJustify, ArrowRight } from 'lucide-react';
 import NavitemSection from './navitem-section';
 import Logo from './logo';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const MobileNav = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const pathname = usePathname();
+
+  React.useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger>
         <AlignJustify className="h-8 w-8 shrink-0" />
       </SheetTrigger>
