7a0bcac9b521bef33bab95de38195c49a00900c4
gourab8389 <deyg6988@gmail.com>
2025-03-10 21:20:07 +0530

    Add ApplyUs hero component and background image; integrate into ApplyUs page

--- src/app/(routes)/apply/page.tsx
+++ src/app/(routes)/apply/page.tsx
@@ -1,10 +1,11 @@
 import { MaxWrapper } from '@/components/shared/max-wrapper'
 import React from 'react'
+import ApplyUsHero from './_components/apply-hero';
 
 const ApplyUsPage = () => {
   return (
     <MaxWrapper className='flex flex-col gap-10 min-h-screen'>
-        page
+        <ApplyUsHero/>
     </MaxWrapper>
   )
 }
