e3b9aa5cfdcb9f139594952110a83286d46295b4
gourab8389 <deyg6988@gmail.com>
2025-03-12 23:19:26 +0530

    Update ApplyUsHero title to reflect correct terminology

--- src/app/(routes)/apply/_components/apply-hero.tsx
+++ src/app/(routes)/apply/_components/apply-hero.tsx
@@ -4,7 +4,7 @@ const ApplyUsHero = () => {
   return (
     <section className='w-full apply-bg aspect-square md:aspect-[3/1] 2xl:aspect-[4/1] flex items-center justify-center pt-10'>
         <div className='text-center'>
-            <h1 className='text-6xl 2xl:text-7xl font-semibold text-white'>Apply for Franchise</h1>
+            <h1 className='text-6xl 2xl:text-7xl font-semibold text-white'>Apply for Branch</h1>
         </div>
     </section>
   )
